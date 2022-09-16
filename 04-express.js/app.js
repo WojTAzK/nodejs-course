db1bed80484cf16a67bba283b0e8f62958037cc7
Wojciech Kowal <w.kowal202@gmail.com>
2022-09-16 12:56:48 +0200

    added simple routing, finished asignment

--- 04-express.js/app.js
+++ 04-express.js/app.js
@@ -1,16 +1,16 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next();
+app.use('/add-product', (req, res, next) => {
+  console.log('Another middleware!!');
+  res.send('<h1>Add product page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('Another middleware!!');
   res.send('<h1>Hello there from Express</h1>');
 });
 
-http.createServer(app).listen(3000);
+app.listen(3000);
