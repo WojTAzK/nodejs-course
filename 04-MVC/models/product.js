6ee6d6453548538104d668e216b0129f28560775
Wojciech Kowal <w.kowal202@gmail.com>
2022-09-22 10:49:40 +0200

    finished basic mvc project

--- 04-MVC/models/product.js
+++ 04-MVC/models/product.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+//////////////////////////////////////
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile((products) => {
       products.push(this);
 
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -28,19 +33,7 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return [];
-      }
-
-      return JSON.parse(fileContent);
-    });
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
