08fb38858a723718c554bdb8458e2fd7a5eb0b8e
Wojciech Kowal <w.kowal202@gmail.com>
2022-09-22 18:40:13 +0200

    finished project no. 4

---
+++ 04-MVC/controllers/admin.js
@@ -0,0 +1,36 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    formsCSS: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const { title, imageUrl, price, description } = req.body;
+  const product = new Product(title, imageUrl, price, description);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+  });
+};
+
+exports.getAdminProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  });
+};
