51dbab2ff932ea2f3d9b69939b4d397b1ad8a94c
Wojciech Kowal <w.kowal202@gmail.com>
2022-09-23 20:06:55 +0200

    added product details page

--- 05-dynamic-routes__advanced-models/models/product.js
+++ 05-dynamic-routes__advanced-models/models/product.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename),
+  path.dirname(require.main.filename),
   'data',
   'products.json'
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -26,9 +26,10 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -36,5 +37,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
