ae5634ff1350f17266c9235e6d5217a9dac8cbc8
Wojciech Kowal <w.kowal202@gmail.com>
2022-09-28 19:06:15 +0200

    added mysql to project

--- 05-dynamic-routes__advanced-models/controllers/admin.js
+++ 05-dynamic-routes__advanced-models/controllers/admin.js
@@ -14,8 +14,10 @@ exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
 
   const product = new Product(id, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product
+    .save()
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
