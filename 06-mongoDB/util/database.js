c29f84f7b1fb95175e01e0e8c68c3809bbcd74fe
Wojciech Kowal <w.kowal202@gmail.com>
2022-10-02 15:29:22 +0200

    connecting to db works :)

--- 06-mongoDB/util/database.js
+++ 06-mongoDB/util/database.js
@@ -1,19 +1,30 @@
-// const mysql = require('mysql2');
+const { get } = require('http');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   database: 'node-complete',
-//   password: '*Kowal202*',
-// });
+let _db;
 
-// module.exports = pool.promise();
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://editor:FamilyGuy@node-course.9vcclgg.mongodb.net/shop  ?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!!!');
+      _db = client.db();
+      callback(client);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
-const Sequelize = require('sequelize');
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
 
-const sequelize = new Sequelize('node-complete', 'root', '*Kowal202*', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+  throw new Error('No database found!');
+};
 
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
