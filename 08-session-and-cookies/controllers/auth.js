46c7a4e474f2452ed4f9a56b8548fc778965f5f7
Wojciech Kowal <w.kowal202@gmail.com>
2022-11-11 10:30:40 +0100

    Add sessions

--- 08-session-and-cookies/controllers/auth.js
+++ 08-session-and-cookies/controllers/auth.js
@@ -1,13 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie')); // getting cookie
+  // console.log(req.get('Cookie')); // getting cookie
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Log In',
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.cookie('loggedIn', true, { httpOnly: true }); // setting cookie
+  // res.cookie('loggedIn', true, { httpOnly: true }); // setting cookie
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
