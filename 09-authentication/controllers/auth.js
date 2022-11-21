7cbd485efea79396d8977e556ece4e5544774f40
Wojciech Kowal <w.kowal202@gmail.com>
2022-11-21 22:04:19 +0100

    add signin functionality

--- 09-authentication/controllers/auth.js
+++ 09-authentication/controllers/auth.js
@@ -18,15 +18,33 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('634c222ae71fced77ea3451e')
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // User.findById('634c222ae71fced77ea3451e')
+  User.findOne({ email: email })
     .then((user) => {
-      console.log(user);
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) return res.redirect('/login');
+
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+
+          return res.redirect('/login');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch((err) => console.log(err));
 };
@@ -39,19 +57,20 @@ exports.postSignup = (req, res, next) => {
     .then((userDoc) => {
       if (userDoc) return res.redirect('/signup');
 
-      return bcrypt.hash(password, 12);
-    })
-    .then((hashedPassword) => {
-      const user = new User({
-        email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
 
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect('/login');
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+        });
     })
     .catch((err) => console.log(err));
 };
