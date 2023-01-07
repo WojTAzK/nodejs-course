ab9efb7977a5cfe265bc9719b1129fb80153889b
Wojciech Kowal <w.kowal202@gmail.com>
2023-01-07 08:14:34 +0100

    Add reset password view Add reset password controller Register reset password route Adjust login view

--- 09-authentication/controllers/auth.js
+++ 09-authentication/controllers/auth.js
@@ -115,3 +115,11 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  res.render('/auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: req.flash('error')[0],
+  });
+};
